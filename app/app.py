73e3efa92119ced9df598bc1528fb4570f6ead7a
firesal <sreejithtdas@ymail.com>
2024-05-24 20:09:05 +0530

    first draft

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def main_app():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.png', mimetype='favicon.png')
+# npx tailwindcss -i ./static/input.css -o ./static/output.css --watch
